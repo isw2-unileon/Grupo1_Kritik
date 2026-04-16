f6d5f35a55ed7e398ac8917f5c4f254498a7c566
AdroVP <avalep01@estudiantes.unileon.es>
2026-04-16 13:16:53 +0200

    Feature #10: Added the rest of the test

--- backend/bd/supabase_test.go
+++ backend/bd/supabase_test.go
@@ -1,35 +1,16 @@
 package bd
 
 import (
-	"fmt"
 	"testing"
 )
 
+/*
+ =========================================================
+ User functions
+ =========================================================
+*/
+
 // GET USER
-func TestGetUserByID(t *testing.T) {
-	user, err := GetUserByID("14")
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	if user == nil {
-		t.Errorf("User is nil")
-	}
-}
-
-func TestGetUserByIDNotFound(t *testing.T) {
-	user, err := GetUserByID("0")
-
-	if err == nil {
-		t.Errorf("Expected error")
-	}
-
-	if user != nil {
-		t.Errorf("User should be nil")
-	}
-}
-
 func TestGetUserByEmail(t *testing.T) {
 	user, err := GetUserByEmail("testuser@gmail.com")
 
@@ -73,9 +54,9 @@ func TestAddUser(t *testing.T) {
 	}
 }
 
-func TestAddUserAlreadyExists(t *testing.T) {
+func TestAddUserMissingData(t *testing.T) {
 	newUser := User{
-		Email:    "testuser@gmail.com",
+		Email:    "testuser2@gmail.com",
 		Password: "testpassword",
 	}
 
@@ -91,47 +72,6 @@ func TestAddUserAlreadyExists(t *testing.T) {
 }
 
 // DELETE USER
-func TestDeleteUserByID(t *testing.T) {
-	userToDelete := User{
-		Email:    "usertodelete@gmail.com",
-		Name:     "usertodelete",
-		Password: "testpassword",
-	}
-
-	addedUser, err := AddUser(userToDelete)
-
-	if err != nil {
-		t.Errorf("Could not add user to delete")
-	}
-
-	if addedUser == nil {
-		t.Errorf("Added user is nil")
-		return
-	}
-
-	isDeleted, err := DeleteUserByID(fmt.Sprintf("%d", addedUser.ID))
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	if !isDeleted {
-		t.Errorf("Function returned false")
-	}
-}
-
-func TestDeleteUserByIDError(t *testing.T) {
-	isDeleted, err := DeleteUserByID("0")
-
-	if err == nil {
-		t.Error(err)
-	}
-
-	if isDeleted {
-		t.Errorf("Function returned true")
-	}
-}
-
 func TestDeleteUserByEmail(t *testing.T) {
 	userToDelete := User{
 		Email:    "usertodelete@gmail.com",
@@ -173,6 +113,209 @@ func TestDeleteUserByEmailError(t *testing.T) {
 	}
 }
 
+// UPDATE USER
 func TestUpdateUserInfo(t *testing.T) {
+	newUserInfo := User{
+		Name: "testuser2",
+	}
 
+	updatedUser, err := UpdateUserInfo("testuser@gmail.com", newUserInfo)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if updatedUser == nil {
+		t.Errorf("Updated user is nil")
+	}
+}
+
+func TestUpdateUserInfoError(t *testing.T) {
+	newUserInfo := User{
+		Name: "testuser2",
+	}
+
+	updatedUser, err := UpdateUserInfo("dhfdskhjdfsj", newUserInfo) // User not in BD
+
+	if err == nil {
+		t.Error("An error was expected")
+	}
+
+	if updatedUser != nil {
+		t.Errorf("Updated user should be nil")
+	}
+}
+
+/*
+ =========================================================
+ Content functions
+ =========================================================
+*/
+
+// GET CONTENT
+func TestGetContentByName(t *testing.T) {
+	content, err := GetContentByName("testcontent")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if content == nil {
+		t.Errorf("Content is nil")
+	}
+}
+
+func TestGetContentByNameNotFound(t *testing.T) {
+	content, err := GetContentByName("0")
+
+	if err == nil {
+		t.Errorf("Expected error")
+	}
+
+	if content != nil {
+		t.Errorf("Content should be nil")
+	}
+}
+
+// ADD CONTENT
+func TestAddContent(t *testing.T) {
+	newContent := Content{
+		Name:  "testcontent",
+		Type:  "Film",
+		Grade: 10,
+	}
+
+	addedContent, err := AddContent(newContent)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if addedContent == nil {
+		t.Errorf("Added content is nil")
+	}
+}
+
+func TestAddContentMissingData(t *testing.T) {
+	newContent := Content{
+		Type:  "Film",
+		Grade: 10,
+	}
+
+	addedContent, err := AddContent(newContent)
+
+	if err == nil {
+		t.Errorf("An error was espected")
+	}
+
+	if addedContent != nil {
+		t.Errorf("Added content should be nil")
+	}
+}
+
+// DELETE CONTENT
+func TestDeleteContentByName(t *testing.T) {
+	contentToDelete := Content{
+		Name:  "contenttodelete",
+		Type:  "Series",
+		Grade: 5,
+	}
+
+	addedContent, err := AddContent(contentToDelete)
+
+	if err != nil {
+		t.Errorf("Could not add content to delete")
+	}
+
+	if addedContent == nil {
+		t.Errorf("Added content is nil")
+		return
+	}
+
+	isDeleted, err := DeleteContentByName(addedContent.Name)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !isDeleted {
+		t.Errorf("Function returned false")
+	}
+}
+
+func TestDeleteContentByNameError(t *testing.T) {
+	isDeleted, err := DeleteContentByName("0")
+
+	if err == nil {
+		t.Error(err)
+	}
+
+	if isDeleted {
+		t.Errorf("Function returned true")
+	}
+}
+
+// UPDATE CONTENT
+func TestUpdateContentInfo(t *testing.T) {
+	newContentInfo := Content{
+		Type: "Series",
+	}
+
+	updatedContent, err := UpdateContentInfo("testcontent", newContentInfo)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if updatedContent == nil {
+		t.Errorf("Updated content is nil")
+	}
+}
+
+func TestUpdateContentInfoError(t *testing.T) {
+	newContentInfo := Content{
+		Name: "testcontent2",
+	}
+
+	updatedContent, err := UpdateContentInfo("dhfdskhjdfsj", newContentInfo) // Content not in BD
+
+	if err == nil {
+		t.Error("An error was expected")
+	}
+
+	if updatedContent != nil {
+		t.Errorf("Updated content should be nil")
+	}
+}
+
+/*
+ =========================================================
+ Hash functions
+ =========================================================
+*/
+
+func TestHashPassword(t *testing.T) {
+	password := "testpassword"
+
+	hashedPassword, err := HashPassword(password)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !VerifyPassword(password, hashedPassword) {
+		t.Error("Passwords do not match")
+	}
+}
+
+func TestHashEmptyPassword(t *testing.T) {
+	password, err := HashPassword("")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if password != "" {
+		t.Error("Password should be empty")
+	}
 }
